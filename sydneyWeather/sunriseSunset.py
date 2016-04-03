f07a3350b178861c630f098795dc8a071fb2a0a6
Kai Wang <kai.wang.nankai@gmail.com>
2016-04-03 16:18:28 +1000

    add comments to sydneyWeather program

--- sydneyWeather/sunriseSunset.py
+++ sydneyWeather/sunriseSunset.py
@@ -1,16 +1,45 @@
 import requests, xlsxwriter
 from bs4 import BeautifulSoup
 
-form = 'END=END&loc=yes&loc=no&lathemi=north&lathemi=south&longhemi=west&longhemi=east&Location=SYDNEY++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++&LatDeg=-33&LatMin=52&LatSec=0&LongDeg=151&LongMin=12&LongSec=0&State=NSW+-+Sydney+%28most+location%29&DST=Yes&TZ=10&UTCOFF=11&ATZ=%28EDT%29&LT=11%3A46%3A01+AM&TimeZoneName=+&austzone=%2B10.5&TimeZone=%2B10.5&Event=7&Event=1&ZenithDeg=&ZenithMin=&ZenithSec=0&height=0&END=END&Date=2016&END=END'
+'''
+This program acquires the sunrise and sunset time of Sydney throughout the entire year of 2016 using the sunrise and sunset computation program by Geoscience Australia.
+Cleaning the raw data from Internet, writing rise and set times to a text file and an excel workbook.
+Extract rise hour, rise minute, set hour, set minute, construct sunrise time, sunset time and compute day length from the difference between sunrise time and sunset time
+Generate scatter plot to show sunrise/sunset time and day length
+	- Show day length on secondary y axis
+	- chart title
+	- axis title
+	- legend
+'''
+
+# form to be submitted
+form = 'END=END&loc=yes&loc=no&lathemi=north&lathemi=south&longhemi=west&longhemi=east&Location=SYDNEY++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++&LatDeg=-33&LatMin=52&LatSec=0&LongDeg=151&LongMin=12&LongSec=0&State=NSW+-+Sydney+%28most+location%29&DST=Yes&TZ=10&UTCOFF=11&ATZ=%28EDT%29&LT=11%3A46%3A01+AM&TimeZoneName=+&austzone=%2B10&TimeZone=%2B10&Event=7&Event=1&ZenithDeg=&ZenithMin=&ZenithSec=0&height=0&END=END&Date=2016&END=END'
+
+# url to Geoscience Australia website
 url = 'http://www.ga.gov.au/bin/geodesy/run/sunrisenset'
 
+# acquire content from website
 content = requests.post(url, data = form).text
 
+# create BeautifulSoup object
 soup = BeautifulSoup(content, 'html.parser')
 
+# raw data in pre tag
 rawData = soup.find('pre').string.encode('utf8')
 
+# ignore metadata at the top
 rawData = rawData.split('\n')[5:]
+
+# format conversion of raw data, putting sunrise and sunset times in two columns in the order of time
+
+'''
+rise set
+XXXX XXXX # Jan 1
+XXXX XXXX # Jan 2
+......
+XXXX XXXX # Dec 30
+XXXX XXXX # Dec 31
+'''
 
 tmpData = []
 
@@ -18,62 +47,139 @@ for row in rawData:
 	if row:
 		tmpData.append(row.replace('                 ', '    XXXX XXXX    ').replace('    ', '  ').split('  ')[1:])
 
+# raw data are presented in two halves, Jan - Jun and Jul to Dec
+# use firstHalf and secondHalf to store
+# data from Jan to Jun
 firstHalf = tmpData[2:33]
+# data from Jul to Dec
 secondHalf = tmpData[35:66]
 
+# get data from firstHalf, add to tmpData
 tmpData = [[] for _ in range(12)]
 for row in firstHalf:
 	for colIdx in range(6):
 		if row[colIdx] != 'XXXX XXXX' and row[colIdx] != '':
 			tmpData[colIdx].append(row[colIdx])
 
+# get data from secondHalf, add to tmpData
 for row in secondHalf:
 	for colIdx in range(6):
 		if row[colIdx] != 'XXXX XXXX' and row[colIdx] != '':
 			tmpData[colIdx + 6].append(row[colIdx])
 
+# generate processed data containing sunrise and sunset times
+# XXXX XXXX
 processed = []
 for row in tmpData:
 	processed += row
 
-# # f = open('sunriseSunset.txt', 'w+')
-# # f.write('\n'.join(processed))
-# # f.close()
+'''
+Saving processed data to a text file (optional)
+'''
 
-# f = open('sunriseSunset.txt')
-# processed = f.read().split('\n')
+'''
+f = open('sunriseSunset.txt', 'w+')
+f.write('\n'.join(processed))
+f.close()
+'''
 
+# initialise sunriseRaws and sunsetRaws arrays to collect raw rise and set data
 sunriseRaws, sunsetRaws = [], []
-sunriseHours, sunriseMinutes = [], []
-sunsetHours, sunsetMinutes = [], []
+
+# create workbook
+wb = xlsxwriter.Workbook('sunriseSunset.xlsx')
+# create sheet
+sheet = wb.add_worksheet()
+
+# write column headers
+sheet.write(0, 0, 'Date')
+sheet.write(0, 1, 'rise')
+sheet.write(0, 2, 'set')
+sheet.write(0, 3, 'rise hour')
+sheet.write(0, 4, 'rise minute')
+sheet.write(0, 5, 'set hour')
+sheet.write(0, 6, 'set minute')
+sheet.write(0, 7, 'Sunrise Time')
+sheet.write(0, 8, 'Sunset Time')
+sheet.write(0, 9, 'Day Length')
+
+# row number
+row = 1
+
+# integer corresponding to 01/01/2016
+day = 42370
+
+# date and time format
+day_format = wb.add_format({'num_format': 'dd/mm/yy'})
+time12_format = wb.add_format({'num_format': 'HH:MM AM/PM'})
+time24_format = wb.add_format({'num_format': 'HH:MM'})
+
+# iterate each processed array
 
 for entry in processed:
 	sunriseRaw, sunsetRaw = entry.split()
 	sunriseRaws.append(sunriseRaw)
 	sunsetRaws.append(sunsetRaw)
 	
-	# sunriseHour, sunriseMinute = sunriseRaw[:2], sunriseRaw[2:]
-	# sunsetHour, sunsetMinute = sunsetRaw[:2], sunsetRaw[2:]
-	# sunriseHours.append(int(sunriseHour))
-	# sunriseMinutes.append(int(sunriseMinute))
-	# sunsetHours.append(int(sunsetHour))
-	# sunsetMinutes.append(int(sunsetMinute))
+	# write date
+	sheet.write('A' + str(row + 1), day, day_format);
 
+	# extract hour and minute from column B, C and write to column D, E, F, G
+	# extract
+	sheet.write_formula(row, 3, 'left(B' + str(row + 1) + ', 2)')
+	sheet.write_formula(row, 4, 'right(B' + str(row + 1) + ', 2)')
+	sheet.write_formula(row, 5, 'left(C' + str(row + 1) + ', 2)')
+	sheet.write_formula(row, 6, 'right(C' + str(row + 1) + ', 2)')
+	# write
+	sheet.write_formula(row, 7, 'time(D' + str(row + 1) + ', E' + str(row + 1) + ', 0)', time12_format)
+	sheet.write_formula(row, 8, 'time(F' + str(row + 1) + ', G' + str(row + 1) + ', 0)', time12_format)
+	sheet.write_formula(row, 9, 'I' + str(row + 1) + ' - H' + str(row + 1), time24_format)
+	
+	row += 1
+	day += 1
 
-wb = xlsxwriter.Workbook('sunriseSunset.xlsx')
-sheet = wb.add_worksheet()
-sheet.write(0, 0, 'rise')
-sheet.write(0, 1, 'set')
-sheet.write_column(1, 0, sunriseRaws)
-sheet.write_column(1, 1, sunsetRaws)
+# write sunriseRaws and sunsetRaws from column B, C
+sheet.write_column(1, 1, sunriseRaws)
+sheet.write_column(1, 2, sunsetRaws)
 
-# sheet.write(0, 2, 'rise hour')
-# sheet.write(0, 3, 'rise minute')
-# sheet.write(0, 4, 'set hour')
-# sheet.write(0, 5, 'set minute')
-# sheet.write_column(1, 2, sunriseHours)
-# sheet.write_column(1, 3, sunriseMinutes)
-# sheet.write_column(1, 4, sunsetHours)
-# sheet.write_column(1, 5, sunsetMinutes)
+# add scatter - smooth chart
 
+chart = wb.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+
+# add sunrise time
+chart.add_series({
+	'name': '=Sheet1!$H$1',
+	'categories': '=Sheet1!$A$2:$A$' + str(len(sunriseRaws) + 1),
+	'values': '=Sheet1!$H$2:$H$' + str(len(sunriseRaws) + 1),
+	})
+
+# add sunset time
+chart.add_series({
+	'name': '=Sheet1!$I$1',
+	'categories': '=Sheet1!$A$2:$A$' + str(len(sunriseRaws) + 1),
+	'values': '=Sheet1!$I$2:$I$' + str(len(sunriseRaws) + 1),
+	})
+
+# add day length
+chart.add_series({
+	'name': '=Sheet1!$J$1',
+	'categories': '=Sheet1!$A$2:$A$' + str(len(sunriseRaws) + 1),
+	'values': '=Sheet1!$J$2:$J$' + str(len(sunriseRaws) + 1),
+	'y2_axis': 1,
+	})
+
+# set title, axis, legend
+chart.set_title({'name': 'Sunrise Time, Sunset Time & Day Length'})
+chart.set_x_axis({'name': 'Date', 'min': 42370, 'max': 42735})
+chart.set_y_axis({'name': 'Sunrise/Sunset Time'})
+chart.set_y2_axis({'name': 'Day Length', 'min': 0.35, 'max': 0.65})
+chart.set_legend({'position': 'bottom'})
+
+# set chart size
+chart.set_size({'x_scale': 1.5, 'y_scale': 1.5 })
+
+# insert chart to a certain position
+sheet.insert_chart('C10', chart)
+
+# close workbook
 wb.close()
--- sydneyWeather/sydneyWeather.py
+++ sydneyWeather/sydneyWeather.py
@@ -1,54 +1,119 @@
 import urllib2, xlsxwriter, datetime
-from bs4 import BeautifulSoup
+
+'''
+This program acquires the daily weather observation of Sydney of the user specified year and month and write it to an excel workbook.
+Meta data are saved in a separate text file.
+Raw data are presented on Sheet1 and a wind rose showing the dominant wind direction is on Sheet2.
+
+Library dependency: urllib2, xlsxwriter, datetime
+'''
 
 def getWeatherData(year, month):
 
+	# customise year month
 	yearMonth = '%04d' % year + '%02d' % month
 
+	# url to csv file
 	url = 'http://www.bom.gov.au/climate/dwo/' + yearMonth + '/text/IDCJDW2124.' + yearMonth + '.csv'
 
+	# parse csv file to get raw data
 	raw = urllib2.urlopen(url).read().split('\r\n')
 
+	# separate meta data
 	meta = '\n'.join(raw[:8])
 	f = open('IDCJDW2124_' + yearMonth + '_meta.txt', 'w+')
 	f.write(meta)
 	f.close()
 
+	# data of interest
 	content = [row.split(',')[1:] for row in raw[10:]]
 
+	# define number of rows and number of columns
 	m, n = len(content), len(content[0])
 
+	# create workbook
 	wb = xlsxwriter.Workbook('IDCJDW2124_' + yearMonth + '.xlsx')
-	sheet = wb.add_worksheet()
+	# add Sheet1
+	raw_sheet = wb.add_worksheet()
 
+	# extract column headers
 	headers = raw[9].split(',')[1:]
 
 	# print(headers)
 
+	# replace degree character
 	col = 0
 	for h in headers:
-		sheet.write(0, col, headers[col].replace('\xb0', 'degree ').replace('"', ''))
+		raw_sheet.write(0, col, headers[col].replace('\xb0', 'degree ').replace('"', ''))
 		col += 1
 
+	# define date and time format
 	date_format = wb.add_format({'num_format': 'd/mm/yyyy'})
 	time_format = wb.add_format({'num_format': 'HH:MM'})
 
+	# wind direction table
+	directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
+	# days array to record the number of days corresponding to each direction
+	days = [0] * 16
+	# initialise windSummary dictionary
+	windSummary = dict()
+
+	# write to Sheet1
 	for row in range(1, m + 1):
 		for col in range(n):
+			# 1st column (col = 0) contains dates
 			if col == 0:
 				if content[row - 1][col]:
 					date = datetime.datetime.strptime(content[row - 1][col], '%Y-%m-%d')
-					sheet.write(row, col, date, date_format)
+					raw_sheet.write(row, col, date, date_format)
+
+			# 9th column (col = 9) contains time
 			elif col == 8:
 				if content[row - 1][col]:
 					time = datetime.datetime.strptime(content[row - 1][col], '%H:%M')
-					sheet.write(row, col, time, time_format)
+					raw_sheet.write(row, col, time, time_format)
+			# otherwise, column might contain number or text
 			else:
+				# try to convert content into float
 				try:
-					sheet.write(row, col, float(content[row - 1][col]))
+					raw_sheet.write(row, col, float(content[row - 1][col]))
+				# directly write text to Sheet1
 				except:
-					sheet.write(row, col, content[row - 1][col])
+					raw_sheet.write(row, col, content[row - 1][col])
+			# special consideration for 7th column, record the number of days for each direction
+			if col == 6:
+				windDir = content[row - 1][col]
+				windSummary[windDir] = windSummary.get(windDir, 0) + 1
 
+	
+	# update days array, align days with correct order of directions
+	for i in range(16):
+		d = directions[i]
+		days[i] = windSummary.get(d, 0)
+
+
+	# add Sheet2 for the wind data
+	wind_sheet = wb.add_worksheet()
+	# add headers
+	wind_sheet.write(0, 0, 'Direction')
+	wind_sheet.write(0, 1, 'Days')
+	# add wind data
+	wind_sheet.write_column(1, 0, directions)
+	wind_sheet.write_column(1, 1, days)
+
+	# add chart
+	chart = wb.add_chart({'type': 'radar'})
+	# add series
+	chart.add_series({
+		'name': '=Sheet2!$B$1',
+		'categories': '=Sheet2!$A$2:$A$17',
+		'values': '=Sheet2!$B$2:$B$17',
+	})
+	# add title
+	chart.set_title({'name': 'Wind rose'})
+	# place chart
+	wind_sheet.insert_chart('D3', chart)
 	wb.close()
 
-# getWeatherData(2015, 5)
+if __name__ == '__main__':
+	getWeatherData(2016, 3)
