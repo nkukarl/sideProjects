ea7cf2d800529b80597ba8984f69e95d30ff9b53
Kai Wang <kai.wang.nankai@gmail.com>
2016-04-10 16:30:20 +1000

    add restart feature

--- flappyBird/flappyBird.py
+++ flappyBird/flappyBird.py
@@ -1,9 +1,10 @@
 import pygame, sys, random
 from pygame.locals import *
 
+pygame.mixer.pre_init(frequency = 22050, size = -16, channels = 2, buffer = 512)
 pygame.init()
 
-# function definition
+# <function>
 def getMask(image):
 	mask = []
 	for x in range(image.get_width()):
@@ -12,8 +13,7 @@ def getMask(image):
 			mask[x].append(bool(image.get_at((x, y))[3]))
 	return mask
 
-def checkCrash(birdMask, birdx, birdy, pipeMask, pipex, pipey):
-
+def checkCollide(birdMask, birdx, birdy, pipeMask, pipex, pipey):
 	birdx = int(birdx)
 	birdy = int(birdy)
 
@@ -28,9 +28,34 @@ def checkCrash(birdMask, birdx, birdy, pipeMask, pipex, pipey):
 			if i >= 0 and i < WIDTH and j >= 0 and j < HEIGHT - baseHEIGHT:
 				if pipeMask[i - pipex][j - pipey] and raw[i][j]:
 					return True
-
 	return False
 
+def scoreDisp(score):
+	scoreStr = str(score)
+	numx = 20
+	numy = 20
+	numWidth = 24
+	for num in scoreStr:
+		DISPLAYSURF.blit(numImgs[int(num)], (numx, numy))
+		numx += numWidth
+
+def paraInit():
+	global birdy, velocity, wingOrder, pipex1, pipey1, pipex2, pipey2, pipex1Passed, pipex2Passed, gameOver, score, collide
+	birdy = 10
+	velocity = 0
+	wingOrder = 0
+	pipex1 = 300
+	pipey1 = random.randint(-225, -125)
+	pipex2 = 470
+	pipey2 = random.randint(-225, -125)
+	pipex1Passed = 0
+	pipex2Passed = 0
+	gameOver = 0
+	score = 0
+	collide = False
+# </function>
+
+# <parameter>
 FPS = 30
 fpsClock = pygame.time.Clock()
 WIDTH = 288
@@ -38,53 +63,46 @@ HEIGHT = 512
 DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Flappy Bird')
 
-WHITE = (255, 255, 255)
+birdx = 50
 birdWIDTH = 34
 birdHEIGHT = 24
-birdx = 50
-birdy = 10
 GRAVITY = 0.25
-velocity = 0
 
-birdImgs = [pygame.image.load('bird_down.png').convert_alpha(), pygame.image.load('bird_mid.png').convert_alpha(), pygame.image.load('bird_up.png').convert_alpha()]
-wingOrder = 0
-
-bgImg = pygame.image.load('bg.png').convert_alpha()
-
-pipe_upImg = pygame.image.load('pipe_up.png').convert_alpha()
-pipe_downImg = pygame.image.load('pipe_down.png').convert_alpha()
-
-pipex1 = 300
-pipey1 = random.randint(-225, -125)
-pipex2 = 470
-pipey2 = random.randint(-225, -125)
-pipex1Flag = 0
-pipex2Flag = 0
 pipeGAP = 150
 pipeWIDTH = 52
 pipeHEIGHT = 320
 
-baseImg = pygame.image.load('base.png').convert_alpha()
 baseHEIGHT = 112
 basex = 0
 basey = HEIGHT - baseHEIGHT
+# </parameter>
 
+# <image>
+birdImgs = [pygame.image.load('sprites/bird_down.png').convert_alpha(), pygame.image.load('sprites/bird_mid.png').convert_alpha(), pygame.image.load('sprites/bird_up.png').convert_alpha()]
+
+bgImg = pygame.image.load('sprites/bg.png').convert_alpha()
+
+pipe_upImg = pygame.image.load('sprites/pipe_up.png').convert_alpha()
+pipe_downImg = pygame.image.load('sprites/pipe_down.png').convert_alpha()
 # masks: birdMask has to be defined inside the while loop since birdImg changes over time
-
 pipe_upMask = getMask(pipe_upImg)
 pipe_downMask = getMask(pipe_downImg)
 
+baseImg = pygame.image.load('sprites/base.png').convert_alpha()
 
-hitSound = pygame.mixer.Sound('hit.wav')
-dieSound = pygame.mixer.Sound('die.wav')
-wingSound = pygame.mixer.Sound('wing.wav')
-pointSound = pygame.mixer.Sound('point.wav')
+numImgs = [pygame.image.load('sprites/' + str(i) + '.png').convert_alpha() for i in range(10)]
+# </image>
 
-notHit = 1
+# <audio>
+hitSound = pygame.mixer.Sound('audio/hit.wav')
+dieSound = pygame.mixer.Sound('audio/die.wav')
+wingSound = pygame.mixer.Sound('audio/wing.wav')
+pointSound = pygame.mixer.Sound('audio/point.wav')
+# </audio>
 
-score = 0
-
-numImgs = [pygame.image.load(str(i) + '.png').convert_alpha() for i in range(10)]
+# <initialisation>
+paraInit()
+# </initialisation>
 
 while True:
 
@@ -102,20 +120,23 @@ while True:
 
 	DISPLAYSURF.blit(baseImg, (basex, basey))
 
-	crash = checkCrash(birdMask, birdx, birdy, pipe_upMask, pipex1, pipey1) or checkCrash(birdMask, birdx, birdy, pipe_downMask, pipex1, pipey1 + pipeHEIGHT + pipeGAP) or checkCrash(birdMask, birdx, birdy, pipe_upMask, pipex2, pipey2) or checkCrash(birdMask, birdx, birdy, pipe_downMask, pipex2, pipey2 + pipeHEIGHT + pipeGAP) or birdy < 0 or birdy >= HEIGHT - baseHEIGHT - birdHEIGHT
+	collide = checkCollide(birdMask, birdx, birdy, pipe_upMask, pipex1, pipey1) or checkCollide(birdMask, birdx, birdy, pipe_downMask, pipex1, pipey1 + pipeHEIGHT + pipeGAP) or checkCollide(birdMask, birdx, birdy, pipe_upMask, pipex2, pipey2) or checkCollide(birdMask, birdx, birdy, pipe_downMask, pipex2, pipey2 + pipeHEIGHT + pipeGAP) or birdy <= 0 or birdy >= HEIGHT - baseHEIGHT - birdHEIGHT
 
-	if crash:
-		if notHit:
-			# hitSound.play()
-			notHit = 0
+	if collide:
+		if gameOver == 0:
+			hitSound.play()
+			gameOver = 1
 
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
-
+			elif event.type == KEYDOWN:
+				if event.key == K_SPACE or event.key == K_UP:
+					paraInit()
+					
 	else:
-
+		
 		wingOrder = (wingOrder + 1) % 3
 
 		velocity += GRAVITY
@@ -125,26 +146,26 @@ while True:
 		if pipex1 - 2 < -pipeWIDTH:
 			pipex1 = WIDTH
 			pipey1 = random.randint(-225, -125)
-			pipex1Flag = 0
+			pipex1Passed = 0
 		else:
 			pipex1 = pipex1 - 2
 			if pipex1 + pipeWIDTH < birdx:
-				pipex1Flag += 1
-			if pipex1Flag == 1:
+				pipex1Passed += 1
+			if pipex1Passed == 1:
 				score += 1
-				# pointSound.play()
+				pointSound.play()
 
 		if pipex2 - 2 < -pipeWIDTH:
 			pipex2 = WIDTH
 			pipey2 = random.randint(-225, -125)
-			pipex2Flag = 0
+			pipex2Passed = 0
 		else:
 			pipex2 = pipex2 - 2
 			if pipex2 + pipeWIDTH < birdx:
-				pipex2Flag += 1
-			if pipex2Flag == 1:
+				pipex2Passed += 1
+			if pipex2Passed == 1:
 				score += 1
-				# pointSound.play()
+				pointSound.play()
 
 		# base location looping
 		basex -= 2
@@ -158,15 +179,8 @@ while True:
 			elif event.type == KEYDOWN:
 				if event.key == K_SPACE or event.key == K_UP:
 					velocity = -5
-					# wingSound.play()
+					wingSound.play()
 
-	scoreStr = str(score)
-	numx = 20
-	numy = 20
-	numWidth = 24
-	for num in scoreStr:
-		DISPLAYSURF.blit(numImgs[int(num)], (numx, numy))
-		numx += numWidth
-
+	scoreDisp(score)
 	pygame.display.update()
 	fpsClock.tick(FPS)
